109248335cf93df20d07d56bf5b9321918cc3ce8
Yusuke Kishishita <31816350+bigface0202@users.noreply.github.com>
2020-05-14 22:46:20 +0900

    Adding the pie chart but need more improve...

--- lib/screens/graph_screen.dart
+++ lib/screens/graph_screen.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 // import 'package:provider/provider.dart';
 
 // import '../models/transaction_prov.dart';
 // import '../models/key_and_item_prov.dart';
 
 class GraphScreen extends StatelessWidget {
+  final List<charts.Series> seriesList;
+  final bool animate;
+
+  GraphScreen(this.seriesList, this.animate);
+
+  /// Creates a [PieChart] with sample data and no transition.
+  // factory GraphScreen.withSampleData() {
+  //   return new GraphScreen(
+  //     _createSampleData(),
+  //     // Disable animations for image tests.
+  //     animate: false,
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('This is graph screen!'),
+    return Container(
+      padding: EdgeInsets.all(80),
+      child: charts.PieChart(seriesList,
+          animate: animate,
+          // Add an [ArcLabelDecorator] configured to render labels outside of the
+          // arc with a leader line.
+          //
+          // Text style for inside / outside can be controlled independently by
+          // setting [insideLabelStyleSpec] and [outsideLabelStyleSpec].
+          //
+          // Example configuring different styles for inside/outside:
+          //       new charts.ArcLabelDecorator(
+          //          insideLabelStyleSpec: new charts.TextStyleSpec(...),
+          //          outsideLabelStyleSpec: new charts.TextStyleSpec(...)),
+          defaultRenderer: new charts.ArcRendererConfig(arcRendererDecorators: [
+            new charts.ArcLabelDecorator(
+                labelPosition: charts.ArcLabelPosition.outside)
+          ])),
     );
   }
 }
--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 import './graph_screen.dart';
 import './index_screen.dart';
 import '.././widgets/main_drawer.dart';
+
+/// Sample linear data type.
+class LinearSales {
+  final int year;
+  final int sales;
+
+  LinearSales(this.year, this.sales);
+}
 
 class TabsScreen extends StatefulWidget {
 
@@ -13,6 +22,26 @@ class TabsScreen extends StatefulWidget {
 class _TabsScreenState extends State<TabsScreen> {
   List<Map<String, Object>> _pages;
   int _selectedPageIndex = 0;
+  /// Create one series with sample hard coded data.
+  static List<charts.Series<LinearSales, int>> _createSampleData() {
+    final data = [
+      new LinearSales(0, 100),
+      new LinearSales(1, 75),
+      new LinearSales(2, 25),
+      new LinearSales(3, 5),
+    ];
+
+    return [
+      new charts.Series<LinearSales, int>(
+        id: 'Sales',
+        domainFn: (LinearSales sales, _) => sales.year,
+        measureFn: (LinearSales sales, _) => sales.sales,
+        data: data,
+        // Set a label accessor to control the text of the arc label.
+        labelAccessorFn: (LinearSales row, _) => '${row.year}: ${row.sales}',
+      )
+    ];
+  }
 
   @override
   void initState() {
@@ -22,7 +51,7 @@ class _TabsScreenState extends State<TabsScreen> {
         'title': 'Week Done List',
       },
       {
-        'page': GraphScreen(),
+        'page': GraphScreen(_createSampleData(), false),
         'title': 'Week Done Graph',
       },
     ];
